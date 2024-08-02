d36b019e48a31db6837887a926dac25dbd3aa2ac
bell <mrzbldefreitas@gmail.com>
2024-08-02 15:54:31 -0300

    ta funcionando

--- config/db.js
+++ config/db.js
@@ -4,10 +4,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: 'localhost',
+    user: 'root',
+    password: 'aluno01',
+    database: 'crud',
 });
 
 connection.connect((err) => {
